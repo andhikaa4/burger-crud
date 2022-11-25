8ee749b88b00f397db83cf65c560a47d728bcedb
Andhika <andhika.ardhiansyah19@gmail.com>
2022-11-25 21:52:15 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 import { UserContextProvider } from './components/context/userContext';
 import { QueryClient, QueryClientProvider } from "react-query";
 
